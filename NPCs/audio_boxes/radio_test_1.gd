dfdc288df1c7745860f4c162ee695fd2860db30e
Possmonaut <possmonaut@gmail.com>
2024-01-31 21:39:00 -0500

    added audio export to radio
    also added a temp track from Frosky

--- NPCs/audio_boxes/radio_test_1.gd
+++ NPCs/audio_boxes/radio_test_1.gd
@@ -1,11 +1,10 @@
 extends Node3D
 
-@export var audio: AudioStreamMP3
+@export var audio = ""
 @onready var aud_stream = $AudioStreamPlayer3D
 
-const lsd_mix = preload("res://music/LSD_Project.mp3")
+func _ready() :
+	aud_stream.stream = load(audio)
+	aud_stream.play(0.0);
 
-func _on_ready() :
-	aud_stream.stream = lsd_mix
-	aud_stream.play();
-	
+		
